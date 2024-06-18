71b5f3498b18175bf1566b2714258292924b0e4b
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-06-18 12:55:15 -0300

    deleted dist folder

--- dist/application/settings.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.settings = void 0;
-exports.settings = {
-    JWT_SECRET: process.env.JWT_SECRET || "123",
-    ACCESS_TOKEN_EXPIRY: '20s',
-    REFRESH_TOKEN_EXPIRY: '10s'
-};
-//# sourceMappingURL=settings.js.map
--- dist/routes/post-route.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.postRoute = void 0;
-const express_1 = require("express");
-exports.postRoute = (0, express_1.Router)({});
