502e575526c5ab87858c40fd05b6fa6f01cc6bb3
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-07-14 19:51:24 -0300

    updated router for delete all data: added comments and likes

--- src/routes/testing-router.ts
+++ src/routes/testing-router.ts
@@ -4,6 +4,8 @@ import {PostModel} from "../models/post";
 import {UserModel} from "../models/user";
 import {SessionModel} from "../models/security";
 import {RequestModel} from "../models/request";
+import {CommentModel} from "../models/comment";
+import {LikeCommentModel} from "../models/like";
 
 
 export const testingRouter = Router({})
@@ -15,6 +17,9 @@ testingRouter.delete('/all-data', async (req: Request, res: Response) => {
      await UserModel.deleteMany({});
      await SessionModel.deleteMany({});
      await RequestModel.deleteMany({});
+     await CommentModel.deleteMany({});
+     await LikeCommentModel.deleteMany({});
+
 
      res.sendStatus(204)
 })
